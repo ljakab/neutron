5482dc459628a0a9ca7dc3c618d05622a9bb232c
Bob Melander <bob.melander@gmail.com>
2013-11-26 12:18:24 +0100

    Updated log message.
    Change-Id: If5322b496d4fb8702e503135a4e02bbf464127db

--- quantum/plugins/cisco/l3/db/l3_router_appliance_db.py
+++ quantum/plugins/cisco/l3/db/l3_router_appliance_db.py
@@ -1117,10 +1117,10 @@ class L3_router_appliance_db_mixin(extraroute_db.ExtraRoute_db_mixin):
                     attempts += 1
                     LOG.info(_('Attempt %(attempt)d to find trunk ports for '
                                'hosting entity %(he_id)s failed. Trying '
-                               'again.'),  {'attempt': attempts,
-                                            'he_id': he_id})
+                               'again in %(time)d seconds.'),
+                             {'attempt': attempts, 'he_id': he_id,
+                              'time': SECONDS_BETWEEN_HOSTING_PORT_LOOKSUPS})
                     eventlet.sleep(SECONDS_BETWEEN_HOSTING_PORT_LOOKSUPS)
-                    LOG.info(_('Here we go. The new try.'))
             else:
                 break
         return res[0]
